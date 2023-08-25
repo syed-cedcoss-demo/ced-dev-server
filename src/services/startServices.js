745cb6f333dfc04423c76d314bb52b5e4ccb8f6b
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2023-08-25 15:02:56 +0530

    pm2 configure

--- src/services/startServices.js
+++ src/services/startServices.js
@@ -2,12 +2,11 @@ import { webhooksProcess } from '../utils/bigcomWebhooks.js';
 import resetOTP from './resetOTP.js';
 
 const startServices = () => {
-  console.log('ggg', process.env.NODE_APP_INSTANCE);
   if (process.env.NODE_ENV === 'development') {
     webhooksProcess();
     resetOTP();
   } else {
-    if (process.env.NODE_APP_INSTANCE === 0) {
+    if (process.env.NODE_APP_INSTANCE === '0') {
       webhooksProcess();
       resetOTP();
     }
