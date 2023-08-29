77b40f9ec70b64d33abd19cc6ec161651fc1ad9b
hasnain-cedcoss <syedhasnainmehadi@cedcommerce.com>
2023-08-29 10:54:58 +0530

    cron fix:

--- src/services/startServices.js
+++ src/services/startServices.js
@@ -3,23 +3,15 @@ import resetOTP from './resetOTP.js';
 
 const startServices = () => {
   try {
-    setInterval(() => {
-      resetOTP();
-    }, 1000 * 60 * 15);
-
-    // if (process.env.NODE_ENV === 'development') {
-    //   webhooksProcess();
-    //   resetOTP();
-    // } else {
-    //   if (process.env.NODE_APP_INSTANCE === '0') {
-    //     webhooksProcess();
-    //     resetOTP();
-    //   }
-    //   if (process.env.NODE_APP_INSTANCE === undefined) {
-    //     webhooksProcess();
-    //     resetOTP();
-    //   }
-    // }
+    if (
+      process.env.NODE_ENV === 'development' ||
+      process.env.NODE_APP_INSTANCE === '0' ||
+      process.env.NODE_APP_INSTANCE === undefined
+    ) {
+      setInterval(() => {
+        resetOTP();
+      }, 1000 * 60 * 15);
+    }
   } catch (error) {
     appError(error);
   }
